b7345ed8dfa87d3f8712b362edbab0df675341ac
miaskoo <Call-Miasko987-craptor@yandex.ru>
2022-09-27 17:58:45 +0300

    update mvs19.bat script build

--- mvs19.bat
+++ mvs19.bat
@@ -12,7 +12,7 @@ copy ".\include\freeglut\freeglut.dll" ".\build\mvs19\Release\freeglut.dll"
 copy ".\include\glew\glew32.dll" ".\build\mvs19\Release\glew32.dll"
 copy ".\include\freeglut\freeglut.lib" ".\build\mvs19\freeglut.lib"
 copy ".\include\glew\glew32.lib" ".\build\mvs19\glew32.lib"
-copy ".\include\SOIL\x64\soil.dll" ".\build\mvs19\soil.lib"
+copy ".\include\SOIL\x64\soil.lib" ".\build\mvs19\soil.lib"
 cd .\build
 cd .\mvs19
 cmake ..\..\ -G "Visual Studio 16 2019" -A x64 -T v142 -DCMAKE_CONFIGURATION_TYPES="Release;Debug;" -DCMAKE_GENERATOR_PLATFORM=x64 -DPLATFORM=WINDOWS
